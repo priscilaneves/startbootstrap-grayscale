ec5f230b21a142c9f26aec8141fcfe318d6b8150
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-09-24 23:47:41 -0300

    alter sync to cp the bucket cmd for index.html and  favicon.ico

--- s3cmd.sh
+++ s3cmd.sh
@@ -8,5 +8,5 @@ aws s3 sync dist/font-awesome-4.3.0 s3://xdevel.com.br/font-awesome-4.3.0  --acl
 #not compress
 aws s3 sync dist/img s3://xdevel.com.br/img  --acl=public-read --delete --cache-control="max-age=86400"
 aws s3 sync dist/fonts s3://xdevel.com.br/fonts  --acl=public-read --delete --cache-control="max-age=86400"
-aws s3 sync dist/index.html s3://xdevel.com.br/index.html  --acl=public-read --delete --cache-control="max-age=86400"
-aws s3 sync dist/favicon.ico s3://xdevel.com.br/favicon.ico  --acl=public-read --delete --cache-control="max-age=86400"
+aws s3 cp   dist/*.html s3://xdevel.com.br/*.html  --acl=public-read --delete --cache-control="max-age=86400"
+aws s3 cp   dist/*.ico s3://xdevel.com.br/*.ico  --acl=public-read --delete --cache-control="max-age=86400"
